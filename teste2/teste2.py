9c856eaf19beb00245ccc19938b454f6048d37f7
Henrique Silva <henrique.silva@lnls.br>
2017-10-04 19:32:10 -0300

    [teste2] Adiciona rotina de decodificacao de palavras com ruido

--- teste2/teste2.py
+++ teste2/teste2.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
+import itertools
+
 
 Fs = float(1000)
-f = 1
+f = 10
 x = (np.arange(Fs))/Fs
 x_p = (np.arange(Fs*4))/Fs
 
-
+#Gera os sinais de todas as letras
 sig_M = [ np.sin(2*np.pi*f * i) for i in x]
 sig_O = [ np.sin(2*np.pi*(f*2) * i) for i in x]
 sig_A = [ np.cos(2*np.pi*f * i) for i in x]
 sig_L = [ np.cos(2*np.pi*(f*2) * i) for i in x]
 
+#Cria dicionario com a letras e seus respectivos codigos
 codigos = {'M':sig_M, 'O':sig_O, 'A':sig_A, 'L':sig_L}
 
 def codifica( palavra ):
@@ -25,16 +29,56 @@ def codifica( palavra ):
     return buf
 
 
-t1 = codifica('LOMA')
-t2 = codifica('AMOL')
-t3 = codifica('MOLA')
+#Deteccao de pico: https://dsp.stackexchange.com/questions/22923/dominating-frequency-in-a-clear-50-hz-signal
+def decodifica_fft( buf ):
+    palavra = []
+    split_buf = np.reshape(buf, (4, len(buf)/4))
+    for letra in split_buf:
+        fft_letra = np.fft.fft(letra)
+        freqs = np.fft.fftfreq(len(letra),(1/Fs))
+        freqs = freqs[np.where(freqs >= 0)]
+        mag = np.abs(fft_letra[np.where(freqs >= 0)])
+        peak = np.argmax(mag)
+        print('Pico: '+str(peak)+' Hz')
+        if peak == f:
+            palavra.append('M')
+        elif peak == (2*f):
+            palavra.append('O')
+        else:
+            print('[ERRO] Frequencia nao indexada: '+str(peak)+' Hz')
+    return ''.join(palavra)
 
-plt.figure()
-plt.plot(t1)
-plt.figure()
-plt.plot(t2)
-plt.figure()
-plt.plot(t3)
-plt.show()
+def decodifica( buf ):
+    palavra = []
+    match = 0
+    split_buf = np.reshape(buf, (4, len(buf)/4))
+    for chunk in split_buf:
+        for key in codigos:
+            result = np.subtract(chunk, codigos[key])
+            #print(np.std(result))
+            if (np.std(result) < 0.7):
+                palavra.append(key)
+                match = 1
+        if match == 0:
+            palavra.append("_")
+    return ''.join(palavra)
 
-t4 = codifica('FOLA')
+#Ruido Branco
+noise = np.random.normal(0, 1, size=4000)/1.44
+
+#Cria todas a permutacoes possiveis dos simbolos
+permut = list(itertools.permutations(list('LOMA')))
+permut = [ ''.join(palavra) for palavra in permut]
+
+#Codifica todas as palavras
+permut_cod = [ codifica(palavra) for palavra in permut ]
+
+#Adiciona ruido
+permut_noise = [ np.add(sinal, noise) for sinal in permut_cod ]
+
+
+for sig in permut_cod:
+    print('Palavra decodificada: '+decodifica(sig))
+
+for sig in permut_noise:
+    print('Palavra decodificada (n): '+decodifica(sig))

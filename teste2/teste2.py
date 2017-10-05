882fdb13869b4ffd9c146acc68e63689416caae0
Henrique Silva <henrique.silva@lnls.br>
2017-10-05 17:06:04 -0300

    [teste2] Adiciona parseador de argumentos de linha de comando

--- teste2/teste2.py
+++ teste2/teste2.py
@@ -1,19 +1,28 @@
+#!/usr/bin/env python
+
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import signal
 import itertools
 
+#Parseador de argumentos
+parser = argparse.ArgumentParser()
+parser.add_argument('-s','--sampling', required=False, type=float, default=1000.0, help='Sampling frequency')
+parser.add_argument('-f','--frequency', required=False, type=float, default=10.0, help='Base signal frequency')
+parser.add_argument('-n','--noise_std', required=False, type=float, default=0.0, help='White noise standard deviation')
+parser.add_argument('-p','--plot', required=False, action='store_true', help='Plot coded word graphs')
+parser.add_argument('palavras', nargs='+', type=str, help='Words to encode/decode')
 
-Fs = float(1000)
-f = 10
-x = (np.arange(Fs))/Fs
-x_p = (np.arange(Fs*4))/Fs
+args = parser.parse_args()
+
+#Periodo de 1 ciclo
+x = np.linspace(0, 1/args.frequency, args.sampling)
 
 #Gera os sinais de todas as letras
-sig_M = [ np.sin(2*np.pi*f * i) for i in x]
-sig_O = [ np.sin(2*np.pi*(f*2) * i) for i in x]
-sig_A = [ np.cos(2*np.pi*f * i) for i in x]
-sig_L = [ np.cos(2*np.pi*(f*2) * i) for i in x]
+sig_M = [ np.sin(2*np.pi*args.frequency * i) for i in x]
+sig_O = [ np.sin(2*np.pi*(args.frequency*2) * i) for i in x]
+sig_A = [ np.cos(2*np.pi*args.frequency * i) for i in x]
+sig_L = [ np.cos(2*np.pi*(args.frequency*2) * i) for i in x]
 
 #Cria dicionario com a letras e seus respectivos codigos
 codigos = {'M':sig_M, 'O':sig_O, 'A':sig_A, 'L':sig_L}
@@ -22,7 +31,7 @@ def codifica( palavra ):
     buf = []
     for letra in palavra:
         if letra in codigos:
-            buf = np.concatenate((buf,codigos[letra]))
+            buf = np.concatenate((buf,np.add(codigos[letra], np.random.normal(0, args.noise_std, size=len(x)))))
         else:
             print('[ERRO]: A letra '+letra+' nao faz parte do codigo!')
             return
@@ -50,9 +59,9 @@ def decodifica_fft( buf ):
 
 def decodifica( buf ):
     palavra = []
-    match = 0
     split_buf = np.reshape(buf, (4, len(buf)/4))
     for chunk in split_buf:
+        match = 0
         for key in codigos:
             result = np.subtract(chunk, codigos[key])
             #print(np.std(result))
@@ -73,12 +82,14 @@ permut = [ ''.join(palavra) for palavra in permut]
 #Codifica todas as palavras
 permut_cod = [ codifica(palavra) for palavra in permut ]
 
-#Adiciona ruido
-permut_noise = [ np.add(sinal, noise) for sinal in permut_cod ]
 
+for item in args.palavras:
+    item_cod = codifica(item)
+    result_decod = decodifica(item_cod)
+    print('Palavra decodificada: '+result_decod),
+    if args.plot:
+        plt.figure()
+        plt.plot(item_cod)
+        plt.title('Palavra \"'+item+'\" codificada (adicionado ruido com std:'+str(args.noise_std)+')')
 
-for sig in permut_cod:
-    print('Palavra decodificada: '+decodifica(sig))
-
-for sig in permut_noise:
-    print('Palavra decodificada (n): '+decodifica(sig))
+plt.show()
